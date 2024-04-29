f8400a6929ce7677d0d776da239c1cdab88bc5a3
realshafer <realshafer@gmail.com>
2024-04-29 13:26:11 -0500

    Add files via upload

---
+++ noise/sketch.js
@@ -0,0 +1,28 @@
+let osc, freq, playButton, stopButton;
+
+function setup() {
+  let cnv = createCanvas(100, 100);
+  // cnv.mousePressed(playOscillator);
+  osc = new p5.Noise('white');
+  osc.amp(1);
+  
+  playButton = createButton('play');
+  playButton.style('font-size', '30px');
+  playButton.style('background-color','#00E938');
+  playButton.style('color','#000000');
+  playButton.style('padding','1em');
+  playButton.position(10,10);
+  playButton.mousePressed(function playOscillator(){
+    osc.start();
+  })
+  
+  stopButton = createButton('stop');
+  stopButton.style('font-size', '30px');
+  stopButton.style('background-color','#F80F05');
+  stopButton.style('color','#FDFAFA');
+  stopButton.style('padding','1em');
+  stopButton.position(10,120);
+  stopButton.mousePressed(function stopOscillator(){
+    osc.stop();
+  })
+}
